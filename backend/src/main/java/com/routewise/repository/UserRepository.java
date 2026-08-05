82a99a8822aca2cd4f66210f497e1e8cbb3ebfd5
reju <reju64141@gmail.com>
2026-08-05 15:56:05 +0530

    Added Google OAuth2 Authentication with JWT

---
+++ backend/src/main/java/com/routewise/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.routewise.repository;
+
+import com.routewise.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User>findByEmail(String email);
+}
---
+++ backend/src/main/java/com/routewise/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.routewise.service;
+
+
+import com.routewise.entity.User;
+import com.routewise.repository.UserRepository;
+import com.routewise.security.CustomUserDetails;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return new CustomUserDetails(user);
+    }
+}
+
